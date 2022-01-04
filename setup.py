026960eb2d3f2ee2091d1bef6dfe2940ef648ad1
dalotodo <dalotodo69@gmail.com>
2022-01-04 13:51:19 +0000

    Updated packages info

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["backtrader"]
+requirements = ["backtrader >= 1.9"]
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
@@ -12,7 +12,8 @@ setup(
     author='Pablo Perez',
     author_email='dalotodo69@gmail.com',
     # Needed to actually package something
-    packages=find_packages(),
+    packages=["backcomm"],
+    package_dir={"backcomm":"backcomm"},
     # Needed for dependencies
     install_requires=requirements,
     # *strongly* suggested for sharing
