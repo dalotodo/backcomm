c75e26c82d0ea830103ca30e314596f1d5fae09d
dalotodo <dalotodo69@gmail.com>
2022-01-04 13:17:30 +0000

    Updated setup.py configuration

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["backtrader"]
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
@@ -7,15 +12,15 @@ setup(
     author='Pablo Perez',
     author_email='dalotodo69@gmail.com',
     # Needed to actually package something
-    packages=['backcomm'],
+    packages=find_packages(),
     # Needed for dependencies
-    install_requires=['backtrader'],
+    install_requires=requirements,
     # *strongly* suggested for sharing
     version='0.1.0',
     # The license can be anything you like
     license='MIT',
     description='This package provides commission utility classes to be used with backtrader',
     # We will also need a readme eventually (there will be a warning)
-    long_description=open('README.md').read_text(),
+    long_description=readme,
     long_description_content_type='text/markdown'
 )
